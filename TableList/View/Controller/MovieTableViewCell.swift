1ed003fc9933a9a1786fd62bf08c742a84e444e0
dianagulbe <diaana.gulbe@gmail.com>
2021-08-04 13:56:34 +0300

    update

---
+++ TableList/View/Controller/MovieTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieTableViewCell.swift
+//  TableList
+//
+//  Created by diana.gulbe on 04/08/2021.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+    @IBOutlet weak var MovieLabel: UILabel!
+    @IBOutlet weak var MovieImageView: UIImageView!
+    
+   
+
+}
